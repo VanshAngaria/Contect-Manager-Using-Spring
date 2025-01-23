3c5cc19b6d25da714cbf521bc0935c925f78f29c
VanshAngaria <vanshangaria20@gmail.com>
2025-01-23 19:39:52 +0530

    project dependencies

---
+++ src/main/java/com/contectManager/Contect/Manager/ContectManagerApplication.java
@@ -0,0 +1,13 @@
+package com.contectManager.Contect.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContectManagerApplication.class, args);
+	}
+
+}
